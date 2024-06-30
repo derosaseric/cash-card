a852dea89681acd1f2c8a74572f3c33a526803c3
derosaseric <84354209+derosaseric@users.noreply.github.com>
2024-06-29 23:55:45 -0700

    test: add integration tests for CashCard pagination and sorting
    - Added test to verify returning all CashCards when a list is requested.
    - Added test for verifying the retrieval of a paginated list of CashCards.
    - Added test for verifying the retrieval of a sorted page of CashCards.
    - Added test to ensure default pagination and sorting values are used correctly.

--- src/test/java/example/cash_card/CashCardApplicationTests.java
+++ src/test/java/example/cash_card/CashCardApplicationTests.java
@@ -2,15 +2,19 @@ package example.cash_card;
 
 import com.jayway.jsonpath.DocumentContext;
 import com.jayway.jsonpath.JsonPath;
+import net.minidev.json.JSONArray;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.test.annotation.DirtiesContext;
+
 import java.net.URI;
 
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.annotation.DirtiesContext.*;
 
 /**
  * Integration tests for the CashCard application.
@@ -60,7 +64,11 @@ class CashCardApplicationTests {
 		assertThat(response.getBody()).isBlank();
 	}
 
+	/**
+	 * Test to verify that a new CashCard can be created.
+	 */
 	@Test
+	@DirtiesContext
 	void shouldCreateANewCashCard() {
 		CashCard newCashCard = new CashCard(null, 250.00);
 
@@ -83,5 +91,84 @@ class CashCardApplicationTests {
 
 		Double amount = documentContext.read("$.amount");
 		assertThat(amount).isEqualTo(250.00);
+	}
+
+	/**
+	 * Test to verify that all CashCards are returned when a list is required.
+	 */
+	@Test
+	void shouldReturnAllCashCardsWhenListIsRequested() {
+		// Send a GET request to the /cashcards endpoints and store the response
+		ResponseEntity<String> response = restTemplate.getForEntity("/cashcards", String.class);
+		assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+
+		DocumentContext documentContext = JsonPath.parse(response.getBody());
+
+		// Extract and verify the number of CashCards in the JSON response
+		int cashCardCount = documentContext.read("$.length()");
+		assertThat(cashCardCount).isEqualTo(3);
+
+		// Extract and verify the fields in the JSON response
+		JSONArray ids = documentContext.read("$..id");
+		assertThat(ids).containsExactlyInAnyOrder(99, 100, 101);
+
+		JSONArray amounts = documentContext.read("$..amount");
+		assertThat(amounts).containsExactlyInAnyOrder(123.45, 1.00, 150.00);
+	}
+
+	/**
+	 * Test to verify that a paginated list of CashCards if returned.
+	 */
+	@Test
+	void shouldReturnAPageOfCashCards() {
+		// Send a GET request to the /cashcards endpoint with pagination parameters and store the response
+		ResponseEntity<String> response = restTemplate.getForEntity("/cashcards?page=0&size=1", String.class);
+		assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+
+		DocumentContext documentContext = JsonPath.parse(response.getBody());
+
+		// Extract and verify the number of CashCards in the page
+		JSONArray page = documentContext.read("$[*]");
+		assertThat(page.size()).isEqualTo(1);
+	}
+
+	/**
+	 *
+	 */
+	@Test
+	void shouldReturnASortedPageOfCashCards() {
+		// Send a GET request to the /cashcards endpoint with pagination and sorting parameters and store the response
+		ResponseEntity<String> response = restTemplate.getForEntity("/cashcards?page=0&size=1&sort=amount,desc", String.class);
+		assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+
+		DocumentContext documentContext = JsonPath.parse(response.getBody());
+
+		// Extract and verify the number of CashCards in the page
+		JSONArray read = documentContext.read("$[*]");
+		assertThat(read.size()).isEqualTo(1);
+
+		// Verify that the amount of the first CashCard in the sorted page is correct
+		double amount = documentContext.read("$[0].amount");
+		assertThat(amount).isEqualTo(150.00);
+	}
+
+	/**
+	 * Test to verify that a sorted page of CashCards is returned with default pagination and sorting parameters.
+	 */
+	@Test
+	void shouldReturnASortedPageOfCashCardsWithNoParametersAndUseDefaultValues() {
+		// Send a GET request to the /cashcards endpoint and store the response
+		ResponseEntity<String> response = restTemplate.getForEntity("/cashcards", String.class);
+		assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+
+		DocumentContext documentContext = JsonPath.parse(response.getBody());
+
+		// Extract and verify the number of CashCards in the page
+		JSONArray page = documentContext.read("$[*]");
+		assertThat(page.size()).isEqualTo(3);
+
+		// Verify that the amounts of the CashCards in the page are in ascending order
+		JSONArray amounts = documentContext.read("$..amount");
+		assertThat(amounts).containsExactly(1.00, 123.45, 150.00);
 	}
 }
